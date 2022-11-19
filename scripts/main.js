711f747d551ec43eda3989245bafe92daa0f0769
learningf <118576583+learningf@users.noreply.github.com>
2022-11-19 11:50:14 +1100

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/shahada.png") {
+    myImage.setAttribute("src", "images/shahada2.png");
+  } else {
+    myImage.setAttribute("src", "images/shahada.png");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Welcome to the Khilafa ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Welcome to the Khilafa ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
